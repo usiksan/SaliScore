e494a38836d29a5b239ec05d8f193caa33effbb5
salilab <usiksan@mail.ru>
2022-02-09 23:00:01 +0300

    try use midi

--- SaliScoreAndroidQt/MainWindow.cpp
+++ SaliScoreAndroidQt/MainWindow.cpp
@@ -3,13 +3,30 @@
 #include <QLabel>
 #include <QVBoxLayout>
 
+#ifdef Q_OS_LINUX
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <fcntl.h>
+#endif
+
 MainWindow::MainWindow(QWidget *parent)
   : QMainWindow(parent)
   {
 //  QVBoxLayout *lay = new QVBoxLayout();
 //  lay->addWidget( new QLabel(QStringLiteral("Primer. Hello.") ) );
 //  setLayout( lay );
-  setCentralWidget( new QLabel(QStringLiteral("Primer. Hello") )  );
+#ifdef Q_OS_LINUX
+  int mMidiHandle = open( "/dev/snd/midiC1D0", O_RDONLY | O_NONBLOCK );
+  QString str;
+  if( mMidiHandle >= 0 ) {
+    str = QStringLiteral("Open successfull");
+    }
+  else {
+    str = QStringLiteral("Not open");
+    }
+#endif
+
+  setCentralWidget( new QLabel( str )  );
   }
 
 MainWindow::~MainWindow()
